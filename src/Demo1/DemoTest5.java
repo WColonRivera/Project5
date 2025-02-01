4276c7a8f027657f5c43ae451a719e9f4d94c95b
17874 <17874@DESKTOP-G3LKH5V.lan>
2025-01-31 21:46:13 -0500

    Implemented logic to add 2 variables

--- src/Demo1/DemoTest5.java
+++ src/Demo1/DemoTest5.java
@@ -5,6 +5,12 @@ public class DemoTest5 {
 	public static void main(String[] args) {
 		System.out.println("Java");
 		
+		int a = 10;
+		int b = 20;
+		
+		int result = a + b;
+		
+		System.out.println(result);
 	}
 
 }
